5bb40f45b379ee8355c951e7195ce1df4082abbd
benitdev <benitdev@gmail.com>
2026-01-21 21:03:33 +0700

    fix: ensure outputFileTracing is set to false in nextConfig

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracing: false,
   redirects: async () => {
     return [
       {
